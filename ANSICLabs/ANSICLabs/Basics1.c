77d713c8188cc0779cc3cf416a53e8c1f1d9f35a
Jonathan Lehr <jonathan@aboutobjects.com>
2019-08-12 13:19:15 -0700

    Scaffolding for ANSI C lab

---
+++ ANSICLabs/ANSICLabs/Basics1.c
@@ -0,0 +1,8 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#include "Basics.h"
+
+void RunBasics1(void) {
+    printf("In %s...\n", __func__);
+}
---
+++ ANSICLabs/ANSICLabs/main.c
@@ -0,0 +1,12 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#include <stdio.h>
+#include "Basics.h"
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    RunBasics1();
+    RunBasics2();
+    return 0;
+}
