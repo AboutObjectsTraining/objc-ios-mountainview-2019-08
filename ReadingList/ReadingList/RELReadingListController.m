fc3575717d77aedc848dfdbcf8c2027b2dc14b19
Jonathan Lehr <jonathan@aboutobjects.com>
2019-08-16 13:47:31 -0700

    Added ReadingList app

---
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -0,0 +1,19 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "RELReadingListController.h"
+
+@implementation RELReadingListController
+
+- (IBAction)done:(UIStoryboardSegue *)segue {
+    // TODO: update UI and save changes
+}
+
+- (IBAction)cancel:(UIStoryboardSegue *)segue {
+    
+}
+
+
+- (IBAction)cancelStuff:(UIStoryboardSegue *)unwindSegue { }
+
+@end
---
+++ ReadingList/ReadingListTests/ReadingListTests.m
@@ -0,0 +1,32 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <XCTest/XCTest.h>
+
+@interface ReadingListTests : XCTestCase
+
+@end
+
+@implementation ReadingListTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
